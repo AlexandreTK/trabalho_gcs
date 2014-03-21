516559e085d97b91a9c3072fabdac532b903b6fe
Carlos Henrique Ferreira Oliveira <carloshfoliveira@icloud.com>
2014-03-21 14:57:45 -0300

    Initializing repo

---
+++ src/engine/main_loop.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int init()
+{
+  cout << "Initialization" << endl;
+
+  return 1; 
+}
+
+bool levelFinished()
+{
+  cout << "Is the level over?" << endl;
+  return true;
+}
+
+int nextLevel()
+{
+  cout << "Next level" << endl;
+  return 0;
+}
+
+void shutdown()
+{
+  cout << "Shutdown" << endl;
+}
+
+void updateTimeStep() { }
+void input() { }
+void ia() { }
+void physics() { }
+void update() { }
+void draw() { }
+
+int main()
+{
+  int level = init();
+
+  while(level > 0)
+  {
+    while(true)
+    {
+      updateTimeStep();
+      input();
+      ia();
+      physics();
+      update();
+      draw();
+      if(levelFinished())
+        break;
+
+    }
+
+    level = nextLevel();
+  }
+
+  shutdown();
+  return 0;
+}
