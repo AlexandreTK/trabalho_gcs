ed30cbb667f09056025d85493dba30d18caab0b5
Carlos Henrique Ferreira Oliveira <carloshfoliveira@icloud.com>
2014-04-26 12:55:43 -0300

    Adding CPP file to handle header and its constructor

---
+++ src/character.cpp
@@ -0,0 +1,21 @@
+#include "character.h"
+
+/*
+ * Constructor
+ */
+Character::Character()
+{
+  /*
+   * Initialize offsets
+   */
+  cPosX = 0;
+  cPosY = 0;
+
+  /*
+   * Initialize height and width
+   */
+  cWidth = 0;
+  cHeight = 0;
+}
+
+
