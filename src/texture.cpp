3c6fcf20d27f48d481d61bf2b033dec0262fe73b
Carlos Henrique Ferreira Oliveira <carloshfoliveira@icloud.com>
2014-04-27 17:59:53 -0300

    Adding CPP file to handle header from texture

---
+++ src/texture.cpp
@@ -0,0 +1,92 @@
+#include <string>
+#include <SDL_image.h>
+#include <SDL.h>
+#include <iostream>
+#include "texture.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+Texture::Texture()
+{
+  texture = NULL;
+  tWidth = 0;
+  tHeight = 0;
+}
+
+Texture::~Texture()
+{
+  /*
+   * Deallocates
+   */
+  free();
+}
+
+bool Texture::loadFromFile(string path, SDL_Renderer * wRenderer)
+{
+  free();
+
+  SDL_Texture * newTexture = NULL;
+
+  SDL_Surface * loadedSurface = IMG_Load(path.c_str());
+  if(loadedSurface == NULL)
+  {
+    cout << "Unable to load image: " << SDL_GetError() << endl;
+    return false;
+  }
+
+  SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
+
+  newTexture = SDL_CreateTextureFromSurface(wRenderer, loadedSurface);
+
+  if(newTexture == NULL)
+  {
+    cout << "Unable to create texture: " << SDL_GetError() << endl;
+  }
+  else
+  {
+    tWidth = loadedSurface->w;
+    tHeight = loadedSurface->h;
+  }
+
+  SDL_FreeSurface(loadedSurface);
+
+  texture = newTexture;
+
+  return texture != NULL;
+}
+
+Texture::free()
+{
+  if(texture != NULL)
+  {
+    SDL_DestroyTexture(texture);
+    texture = NULL;
+    tWidth = 0;
+    tHeight = 0;
+  }
+}
+
+Texture::render(int x, int y, SDL_Rect * clip = NULL, double angle = 0.0, SDL_Point * center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE, SDL_Renderer * wRenderer)
+{
+  SDL_Rect renderQuad = {x, y, tWidth, tHeight};
+
+  if(clip != NULL)
+  {
+    renderQuad.w = clip->w;
+    renderQuad.h = clip->h;
+  }
+
+  SDL_RenderCopyEx(wRenderer, texture, clip, &renderQuad, angle, center, flip);
+}
+
+Texture::getWidth()
+{
+  return tWidth;
+}
+
+Texture::getHeight()
+{
+  return tHeight;
+}
--- src/texture.h
+++ src/texture.h
@@ -25,7 +25,7 @@ class Texture
     /*
      * Loads an image from specified path
      */
-    bool loadFromFile(string path);
+    bool loadFromFile(string path, SDL_Renderer * wRenderer);
 
     /*
      * Deallocates texture
@@ -35,7 +35,7 @@ class Texture
     /*
      * Renders texture
      */
-    void render(int x, int y, SDL_Rect * clip = NULL, double angle = 0.0, SDL_Point * center = NULL,  SDL_RendererFlip flip = SDL_FLIP_NONE);
+    void render(int x, int y, SDL_Rect * clip = NULL, double angle = 0.0, SDL_Point * center = NULL,  SDL_RendererFlip flip = SDL_FLIP_NONE, SDL_Renderer * wRenderer);
 
     /*
      * Gets image dimensions
