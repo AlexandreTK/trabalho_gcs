b798008994279b5ecd309648f76b47c499cdf852
Carlos Henrique Ferreira Oliveira <carlospecter@gmail.com>
2014-05-07 14:11:58 -0300

    Adding comments to the Game class

--- src/game.cpp
+++ src/game.cpp
@@ -4,11 +4,24 @@
 using std::cout;
 using std::endl;
 
+/*
+ * Constructor
+ */
 Game::Game()
 {
   this->level = 0;
 }
 
+/*
+ * Destructor
+ */
+Game::~Game()
+{
+}
+
+/*
+ * Initialize game
+ */
 int Game::init()
 {
   cout << "Initialization" << endl;
@@ -16,6 +29,9 @@ int Game::init()
   return 1;
 }
 
+/*
+ * Check whether the level has finished or not
+ */
 bool Game::levelFinished()
 {
   cout << "Is the level over?" << endl;
@@ -23,6 +39,9 @@ bool Game::levelFinished()
   return true;
 }
 
+/*
+ * Go to the next level
+ */
 int Game::nextLevel()
 {
   cout << "Next level?" << endl;
@@ -30,40 +49,67 @@ int Game::nextLevel()
   return 0;
 }
 
+/*
+ * Shutdown the game
+ */
 void Game::shutdown()
 {
   cout << "Shutdown" << endl;
 }
 
+/*
+ * Update the time run since the game's beginning
+ */
 void Game::updateTimeStep()
 {
 }
 
+/*
+ * Loads the input 
+ */
 void Game::input()
 {
 }
 
+/*
+ * Loads the IA of the game
+ */
 void Game::ia()
 {
 }
 
+/*
+ * Loads the physics
+ */
 void Game::physics()
 {
 }
 
+/*
+ * Update the screen of the game
+ */
 void Game::update()
 {
 }
 
+/*
+ * Draw the textures of the game
+ */
 void Game::draw()
 {
 }
 
+/*
+ * Set the level of the game
+ */
 void Game::setLevel(int inputLevel)
 {
   this->level = inputLevel;
 }
 
+/*
+ * Get current level
+ */
 int Game::getLevel()
 {
   return this->level;
