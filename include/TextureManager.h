ab2e7baa66ab89b702ab470a71d4c4ba112d51b5
Carlos Henrique Ferreira Oliveira <carlospecter@gmail.com>
2014-05-08 09:25:43 -0300

    Added functions to handle a texture manager

---
+++ include/TextureManager.h
@@ -0,0 +1,25 @@
+#ifndef TEXTUREMANAGER_H
+#define TEXTUREMANAGER_H
+
+#include <string>
+#include <SDL.h>
+
+using std::string;
+using std::map;
+
+class TextureManager
+{
+  public:
+    TextureManager();
+    ~TextureManager();
+
+    bool load(string fileName, string id, SDL_Renderer * renderer);
+
+    void draw(string id, int x, int y, int w, int h, SDL_Renderer * renderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
+    void drawFrame(string id, int x, int y, int w, int h, int currentRow, int currentFrame, SDL_Renderer * renderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+  private:
+    map<string, SDL_Texture *> textureMap;
+};
+
+#endif
