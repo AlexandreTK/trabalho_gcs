504b1658bde3239f2460256ecf75f11eaf345520
Bruno de Amorim Campos <amorim.software@gmail.com>
2014-05-09 18:05:52 -0300

    Started the Input header

---
+++ include/InputHandler.h
@@ -0,0 +1,44 @@
+#ifndef INPUTHANDLER_H
+#define INPUTHANDLER_H
+
+#include <vector>
+#include <SDL.h>
+
+using std::vector;
+
+/*
+*Creates the class to handdle the input.
+*/
+class InputHandler
+{
+public:
+	
+	static InputHandler* Instance ()
+	{
+		if (inputInstance == 0)
+		{
+			inputInstance = new InputHandler();
+		}
+
+		return inputInstance;
+	}
+
+	void update();
+	void clean();
+	bool joysticksInitialized () {
+		return numberJoysticks;
+	}
+
+private:
+	InputHandler();
+	~InputHandler(){}
+
+	vector<SDL_Joystick*> joysticks;
+	bool numberJoysticks;
+	void initializeJoysticks();
+
+	static InputHandler* inputInstance;
+};
+typedef InputHandler TheInputHandler;
+
+#endif
