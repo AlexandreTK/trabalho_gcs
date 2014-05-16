a02e8ba6f9809739c78d933c6073edba99fcc368
Bruno de Amorim Campos <amorim.software@gmail.com>
2014-05-16 14:26:06 -0300

    Started the input of the joystick in the Player

--- src/Player.cpp
+++ src/Player.cpp
@@ -21,10 +21,44 @@ void Player::draw()
  */
 void Player::update()
 {
-  this->x -= 1;
-  this->currentFrame = int(((SDL_GetTicks() / 100) % 6));
+  /*this->x -= 1;
+  this->currentFrame = int(((SDL_GetTicks() / 100) % 6));*/
+	m_velocity.setX(0);
+	m_velocity.setY(0);
+
+	handleInput(); // add our function
+
+	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
+
+	SDLGameObject::update();
 }
 
 void Player::clean()
 {
+}
+
+void Player::handleInput()
+{
+	if (TheInputHandler::Instance()->joysticksInitialized())
+	{
+		if (TheInputHandler::Instance()->xValue(0, 1) > 0 || TheInputHandler::Instance()->xValue(0, 1) < 0)
+		{
+			m_velocity.setX(1 * TheInputHandler::Instance()->xValue(0,1));
+		}
+
+		if (TheInputHandler::Instance()->yValue(0, 1) > 0 || TheInputHandler::Instance()->yValue(0, 1) < 0)
+		{
+			m_velocity.setY(1 * TheInputHandler::Instance()->yValue(0,1));
+		}
+
+		if (TheInputHandler::Instance()->xValue(0, 2) > 0 || TheInputHandler::Instance()->xValue(0, 2) < 0)
+		{
+			m_velocity.setX(1 * TheInputHandler::Instance()->xValue(0,2));
+		}
+
+		if (TheInputHandler::Instance()->yValue(0, 2) > 0 || TheInputHandler::Instance()->yValue(0, 2) < 0)
+		{
+			m_velocity.setY(1 * TheInputHandler::Instance()->yValue(0,2));
+		}
+	}
 }
