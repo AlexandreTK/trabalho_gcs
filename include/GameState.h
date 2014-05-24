5053984a86a614e57a21173947ba19df6d16c13a
Carlos Henrique Ferreira Oliveira <carlospecter@gmail.com>
2014-05-23 21:25:51 -0300

    Adding the GameState header to handle states such as Menu, Pause, Credits...

---
+++ include/GameState.h
@@ -0,0 +1,26 @@
+#ifndef GAMESTATE_H
+#define GAMESTATE_H
+
+#include <string>
+
+using std::string;
+
+/*
+ * A class to handle the Game States
+ * such as Menu, Levels and Credits
+ */
+
+class GameState
+{
+  public:
+    virtual void update() = 0;
+    virtual void render() = 0;
+
+    virtual bool onEnter() = 0;
+    virtual bool onExit() = 0;
+    
+
+    virtual string getStateID() const = 0;
+};
+
+#endif
