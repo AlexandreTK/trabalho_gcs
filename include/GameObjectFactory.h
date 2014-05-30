8aed14b7a1a54206ad21ecc08aac64fda3800108
Carlos Henrique Ferreira Oliveira <carlospecter@gmail.com>
2014-05-30 12:58:50 -0300

    Making GameObjectFactory a singleton

--- include/GameObjectFactory.h
+++ include/GameObjectFactory.h
@@ -1,27 +1,70 @@
-#ifndef GAMEOBJECT_H
-#define GAMEOBJECT_H
+#ifndef GAMEOBJECTFACTORY_H
+#define GAMEOBJECTFACTORY_H
 
-#include "LoaderParams.h"
-#include <SDL.h>
+#include <string>
+#include <map>
+#include "GameObject.h"
+#include <iostream>
+
 
 
 using std::string;
+using std::map;
+using std::cout;
 
-class GameObject
+class BaseCreator
 {
-  public:
-
-    virtual void draw()=0;
-    virtual void update()=0;
-    virtual void clean()=0;
-
-    //new load function
-    virtual void load(const LoaderParams* pParams) =0;
-
-  protected:
-    GameObject(){}
-    virtual ~GameObject(){}
+public:
+	virtual GameObject* createGameObject() const = 0;
+	virtual ~BaseCreator(){}
+	
 };
 
-#endif
+class GameObjectFactory
+{
+public:
+  ~GameObjectFactory();
+  static GameObjectFactory * Instance()
+  {
+    if(!instance)
+      instance = new GameObjectFactory();
 
+    return instance;
+  }
+	bool registerType(std::string typeID, BaseCreator* pCreator)
+	{
+		map<string, BaseCreator*>::iterator it = m_creators.find(typeID);
+		// if the type is already registered, do nothing
+		if(it != m_creators.end())
+		{
+			delete pCreator;
+			return false;
+		}
+
+		m_creators[typeID] = pCreator;
+		return true;
+	}
+
+	GameObject* create(string typeID)
+	{
+
+		map<string, BaseCreator*>::iterator it =
+		m_creators.find(typeID);
+
+		if(it == m_creators.end())
+		{
+			cout << "could not find type: " << typeID << "\n";
+			return 0;
+		}
+
+		BaseCreator* pCreator = (*it).second;
+		return pCreator->createGameObject();
+	}
+
+private:
+        static GameObjectFactory * instance;
+	map<string, BaseCreator*> m_creators;
+		
+};
+typedef GameObjectFactory TheGameObjectFactory;
+#endif
