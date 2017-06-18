7512bb4405dcb9ff42c5cd07be5fbb1ea0f84e4d
Alexandre <alexandrekry@gmail.com>
2017-06-18 19:33:42 -0300

    small changes to environment.sh

--- environment.sh
+++ environment.sh
@@ -1,5 +1,6 @@
+sudo apt-get update
 echo "Install some required packages for compilation"
-sudo apt-get install make cmake libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-mixer-dev zlib1g-dev  clang build-essential
+sudo apt-get install make cmake  libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-mixer-dev zlib1g-dev clang build-essential
 
 echo "Install some required packages for the debian package"
 sudo apt-get install libc6 libgcc1 libsdl2-2.0-0 libsdl2-image-2.0-0 libstdc++6
